5ff25a9b48e16a10661b6a19a6cd3037df967318
Nabin Islam <sinabin62246@gmail.com>
2022-11-24 14:30:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lqyancf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+function run() {
+  try {
+    const categoriesCollection = client
+      .db('DealFourWheel')
+      .collection('categories');
+
+    app.get('/categories', async (req, res) => {
+      const categories = await categoriesCollection.find({}).toArray();
+      res.send(categories);
+    });
+    
+  } finally {
+  }
+}
+
+run().catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+  res.send('DealFourWheel server is running');
+});
+
+app.listen(port, () => {
+  `DealFourWheel server is running on port:${port}`;
+});
