68c725a46ef598a01033d3816e9abf121ff7cfa0
Nabin Islam <sinabin62246@gmail.com>
2022-11-25 18:44:49 +0600

    get seller and admin status by email done

--- index.js
+++ index.js
@@ -75,6 +75,20 @@ async function run() {
       const user = await usersCollection.findOne(query);
       res.send({ isBuyer: user?.role === 'buyer' });
     });
+
+    app.get('/user/seller/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isSeller: user?.role === 'seller' });
+    });
+
+    app.get('/user/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === 'admin' });
+    });
   } finally {
   }
 }
