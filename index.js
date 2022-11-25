fdd341c9cd1a7a3624bb9c8fdf05da0256fbdd56
Nabin Islam <sinabin62246@gmail.com>
2022-11-25 14:07:56 +0600

    jwt token setup done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+var jwt = require('jsonwebtoken');
 
 const port = process.env.PORT || 5000;
 
@@ -19,12 +20,42 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function varifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send('Unauthorized access');
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: 'forbidden access' });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     const categoriesCollection = client
       .db('DealFourWheel')
       .collection('categories');
     const usersCollection = client.db('DealFourWheel').collection('users');
+
+    app.get('/jwt', async (req, res) => {
+      const email = req.query.email;
+      const user = await usersCollection.findOne({ email: email });
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: '1h',
+        });
+        return res.send({ accessToken: token });
+      }
+
+      res.status(403).send({ accessToken: '' });
+    });
 
     // categories api
     app.get('/categories', async (req, res) => {
